3179a60fed013f338fc12a7a0dac9adc3418223a
Josh Kuhar <jdkuhar@gmail.com>
2016-07-24 21:08:23 -0400

    update

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var Questions = function(question) {
+  this.question = question;
+};
+
+var Ingredients = function(ingredients) {
+  this.ingredients = ingredients;
+};
+
+var Pantry = function(pantryItem) {
+  this.pantry = pantryItem;
+};
