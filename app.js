6dad78af5d658d05a46e8fcc25ca061346a3f860
Josh Kuhar <jdkuhar@gmail.com>
2016-07-27 13:32:40 -0400

    update

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 'use strict';
+//Global variable to keep track of questions
+var questionNumber = 0;
+
+$('.answer').on('click', 'span', function(){
+	
+	if ( $( this ).attr('id') === 'yes') {
+		tryIt(questionNumber);
+	} else {
+		console.log("no");
+	}
+
+	questionNumber++;
+	console.log( $( this ).attr('id') );
+	console.log(questionNumber);
+})
+
+//Drink - this is the drink object
 var Drink = function(){
   this.drinkIngredients = [];
 };
 
+//add() - this function adds an ingredient to the drink
 Drink.prototype.add = function(ingredient) {
   this.drinkIngredients.push(ingredient);
 };
 
+//Pantry - this is the list of drink ingredients
 var Pantry = function(){
   Drink.call(this);
   this.pantryIngredients = [
@@ -21,6 +40,7 @@ var Pantry = function(){
 Pantry.prototype = Object.create(Drink.prototype);
 Pantry.prototype.constructor = Pantry;
 
+//getIngredient() - this function pulls an ingredient from the pantry
 Pantry.prototype.getIngredient = function(questionNumber){
   return this.pantryIngredients[questionNumber][Math.floor((Math.random() * 3) + 1)];
 };
@@ -29,6 +49,7 @@ var pantry = new Pantry();
 
 var Questions = function(){
   this.questions = [
+  	"Are ye curious enough to try one of me drinks?",
 	"Do ye like yer drinks strong?", 
 	"Do ye like it with a salty tang?", 
 	"Are ye a lubber who likes it bitter?", 
@@ -39,11 +60,14 @@ var Questions = function(){
 Questions.prototype.askQuestion = function(questionNumber) {
   return this.questions[questionNumber];
 };
-
-var q = 0;
 var question = new Questions();
-console.log(question.askQuestion(q));
 
-console.log(pantry.getIngredient(q));
+function tryIt(q){
+  pantry.add(pantry.getIngredient(q));
+}
+
+console.log(question.askQuestion(questionNumber));
+
+console.log(pantry.getIngredient(questionNumber));
 
 
