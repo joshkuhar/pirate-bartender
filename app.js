77f333bbae3a8ae948abf122c053de9f74f45690
Josh Kuhar <jdkuhar@gmail.com>
2016-07-27 12:31:06 -0400

    update

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 'use strict';
 var Drink = function(){
-  this.ingredients = [];
+  this.drinkIngredients = [];
 };
 
 Drink.prototype.add = function(ingredient) {
-  this.ingredients.push(ingredient);
+  this.drinkIngredients.push(ingredient);
 };
 
 var Pantry = function(){
   Drink.call(this);
-  this.ingredients = [
-  ["Glug of rum", "slug of whisky", "splash of gin"],
-  ["Olive on a stick", "salt-dusted rim", "rasher of bacon"],
-  ["Shake of bitters", "splash of tonic", "twist of lemon peel"],
-  ["Sugar cube", "spoonful of honey", "splash of cola"],
-  ["Slice of orange", "dash of cassis", "cherry on top"]
+  this.pantryIngredients = [
+  ["Strong", "glug of rum", "slug of whisky", "splash of gin"],
+  ["Salty", "olive on a stick", "salt-dusted rim", "rasher of bacon"],
+  ["Bitter", "shake of bitters", "splash of tonic", "twist of lemon peel"],
+  ["Sweetness", "sugar cube", "spoonful of honey", "splash of cola"],
+  ["Fruity", "slice of orange", "dash of cassis", "cherry on top"]
     ];
 };
 
 Pantry.prototype = Object.create(Drink.prototype);
 Pantry.prototype.constructor = Pantry;
 
-Pantry.prototype.getIngredient = function(number){
-  return this.ingredients[number][0];
+Pantry.prototype.getIngredient = function(questionNumber){
+  return this.pantryIngredients[questionNumber][Math.floor((Math.random() * 3) + 1)];
 };
 
 var pantry = new Pantry();
@@ -40,17 +40,10 @@ Questions.prototype.askQuestion = function(questionNumber) {
   return this.questions[questionNumber];
 };
 
+var q = 0;
 var question = new Questions();
-console.log(question.askQuestion(1));
+console.log(question.askQuestion(q));
 
-
-var Drink = function(){
-  this.ingredient = [];
-};
-
-Drink.prototype.add = function(ingredient) {
-  this.ingredient.push(ingredient);
-};
-
+console.log(pantry.getIngredient(q));
 
 
