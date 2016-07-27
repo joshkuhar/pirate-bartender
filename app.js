2a8f5994fb81441c14c63a7a83d3cb0c315a6c22
Josh Kuhar <jdkuhar@gmail.com>
2016-07-26 21:37:23 -0400

    update

--- app.js
+++ app.js
@@ -26,6 +26,49 @@ var questions = [
 	"Are ye one for a fruity finish?"];
 
 	/*
+var Drink = function(){
+  this.ingredients = [];
+};
+
+Drink.prototype.add = function(ingredient) {
+  this.ingredients.push(ingredient);
+};
+
+var Pantry = function(){
+  Drink.call(this);
+  this.ingredients = [
+  ["Glug of rum", "slug of whisky", "splash of gin"],
+  ["Olive on a stick", "salt-dusted rim", "rasher of bacon"],
+  ["Shake of bitters", "splash of tonic", "twist of lemon peel"],
+  ["Sugar cube", "spoonful of honey", "splash of cola"],
+  ["Slice of orange", "dash of cassis", "cherry on top"]
+    ];
+};
+
+Pantry.prototype = Object.create(Drink.prototype);
+Pantry.prototype.constructor = Pantry;
+
+Pantry.prototype.getIngredient = function(number){
+  return this.ingredients[number][0];
+};
+
+var pantry = new Pantry();
+
+var Questions = function(){
+  this.questions = [
+	"Do ye like yer drinks strong?", 
+	"Do ye like it with a salty tang?", 
+	"Are ye a lubber who likes it bitter?", 
+	"Would ye like a bit of sweetness with yer poison?", 
+	"Are ye one for a fruity finish?"];
+};
+
+Questions.prototype.askQuestion = function(questionNumber) {
+  return this.questions[questionNumber];
+};
+
+var question = new Questions();
+console.log(question.askQuestion(1));
 
 
 var a = ["a", "b", "c", "d"];
@@ -53,6 +96,12 @@ var Pantry = function(preference) {
 	var drink = [];
 	grab preference and add to drink;
 }
+
+  this.strong = ["Glug of rum", "slug of whisky", "splash of gin"];
+  this.salty = ["Olive on a stick", "salt-dusted rim", "rasher of bacon"];
+  this.bitter = ["Shake of bitters", "splash of tonic", "twist of lemon peel"];
+  this.sweet = ["Sugar cube", "spoonful of honey", "splash of cola"];
+  this.fruity = ["Slice of orange", "dash of cassis", "cherry on top"];
 
 var Strong = ["Glug of rum", "slug of whisky", "splash of gin"]
 var Salty = ["Olive on a stick", "salt-dusted rim", "rasher of bacon"]
